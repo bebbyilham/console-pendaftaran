b1a8185dbd86b57719e6b7b775dabf4989c21acc
Bebby Ilham Akbar Aresta <bebbyilham@gmail.com>
2025-04-09 07:54:10 +0700

    0904 0752 cetak struk 3

--- application/modules/dashboard/views/index.php
+++ application/modules/dashboard/views/index.php
@@ -763,6 +763,34 @@
                                                                                                                               });
 
                                                                                                                           });
+
+                                                                                                                          qz.printers.find("Receipt").then(function(printer) {
+
+                                                                                                                              var config = qz.configs.create(printer);
+                                                                                                                              console.log(noregistrasi);
+                                                                                                                              var no_reg = noregistrasi;
+
+
+                                                                                                                              var today = new Date();
+                                                                                                                              var month = today.getMonth() + 1;
+                                                                                                                              var time = today.getDate() + "/" + month + "/" + today.getFullYear() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                                                                                                                              var data = [{
+                                                                                                                                  type: 'pixel',
+                                                                                                                                  format: 'pdf',
+                                                                                                                                  flavor: 'file',
+                                                                                                                                  data: '<?= base_url('dashboard/cetak_registrasi_bpjs/') ?>' + no_reg
+                                                                                                                              }];
+                                                                                                                              return qz.print(config, data);
+                                                                                                                          }).catch(function(e) {
+                                                                                                                              console.log(e);
+
+                                                                                                                              Swal.fire({
+                                                                                                                                  icon: 'error',
+                                                                                                                                  title: 'Printer Tidak Ditemukan',
+                                                                                                                                  text: data.metadata.message,
+                                                                                                                              });
+
+                                                                                                                          });
                                                                                                                           $('#modal-kodebooking-jkn').modal('hide');
                                                                                                                       }, 700);
 
@@ -1221,7 +1249,11 @@
                                                       });
                                                   }
                                               } else {
-
+                                                  Swal.fire({
+                                                      icon: 'error',
+                                                      title: 'Gagal',
+                                                      text: 'Silahkan mendaftar melalui admisi',
+                                                  });
                                               }
 
                                           }
@@ -1867,6 +1899,34 @@
                                                       }).catch(function(e) {
                                                           console.log(e);
                                                           //   alert("Printer Tidak Ditemukan");
+                                                          Swal.fire({
+                                                              icon: 'error',
+                                                              title: 'Printer Tidak Ditemukan',
+                                                              text: data.metadata.message,
+                                                          });
+
+                                                      });
+
+                                                      qz.printers.find("Receipt").then(function(printer) {
+
+                                                          var config = qz.configs.create(printer);
+                                                          console.log(noregistrasi);
+                                                          var no_reg = noregistrasi;
+
+
+                                                          var today = new Date();
+                                                          var month = today.getMonth() + 1;
+                                                          var time = today.getDate() + "/" + month + "/" + today.getFullYear() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                                                          var data = [{
+                                                              type: 'pixel',
+                                                              format: 'pdf',
+                                                              flavor: 'file',
+                                                              data: '<?= base_url('dashboard/cetak_registrasi_bpjs/') ?>' + no_reg
+                                                          }];
+                                                          return qz.print(config, data);
+                                                      }).catch(function(e) {
+                                                          console.log(e);
+
                                                           Swal.fire({
                                                               icon: 'error',
                                                               title: 'Printer Tidak Ditemukan',
